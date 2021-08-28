0eb69cea03d9ae16bdfdced0b900c9326d615a6b
uzor samuel ugochukwu <sammysmart98@yahoo.com>
2021-08-28 22:33:45 +0100

    Feat:add posts  layout page

--- src/.vuepress/components/Posts.vue
+++ src/.vuepress/components/Posts.vue
@@ -1,17 +1,33 @@
 <template>
   <div class="container">
-      <div class="card">
-          
+  <router-link v-for="post in posts" :to="post.path" :key="post.id">
+        <div class="card">
+      <img class="imgage" src="../public/home.png" alt="" />
+      <div class="details">
+        <div class="title">{{ post.title }}</div>
+        <div class="desc">{{ post.frontmatter.description }}</div>
+        <div class="author">{{ post.frontmatter.author }}</div>
       </div>
+    </div>
+  </router-link>
   </div>
 </template>
 
 <script>
 export default {
-
-}
+  data() {
+    return {
+      posts: [],
+    };
+  },
+  mounted() {
+    this.$site.posts.forEach((page) => {
+      if (page.frontmatter.type === "blog") {
+        this.posts.push(post);
+      }
+    });
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
