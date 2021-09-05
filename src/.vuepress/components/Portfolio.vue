2bff50193918cf7e3a90e7f8d896808f1d869e88
uzor samuel ugochukwu <sammysmart98@yahoo.com>
2021-09-05 14:10:19 +0100

    add project card

--- src/.vuepress/components/Portfolio.vue
+++ src/.vuepress/components/Portfolio.vue
@@ -1,17 +1,30 @@
 <template>
-    <div>
-
-        hello felleas
+  <div>
+    <div class="portfolio-card">
+        <div class="project-image"></div>
+        <div class="project-desc"></div>
     </div>
+  </div>
 </template>
 <script>
 export default {
-    data(){
-return{
-    projects : [
-        {}
-    ]
-}
-    }
-}
+  data() {
+    return {
+      projects: [
+        { name: "", description: "" },
+        { name: "", description: "" },
+        { name: "", description: "" },
+      ],
+    };
+  },
+};
 </script>
+<style scoped>
+.portfolio-card{
+    width: 100%;
+    height: 300px;
+    border-radius: 12px;
+    box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+}
+
+</style>
